4c2624785c064f90450fa66ef0a3002b0bfae29e
EvalineBosch <45753169+EvalineBosch@users.noreply.github.com>
2022-02-04 14:13:15 +0100

    Update train.py

--- train.py
+++ train.py
@@ -333,7 +333,7 @@ if __name__ == "__main__":
     # train_data = create_dataset_classes(train_path, 64, ['cat', 'dog'])
     # train_loader = DataLoader(train_data, batch_size=args.batch, shuffle=True)
 
-    train_path_thumb = args.dir + '/Thumbnails128x128/train/'
+    train_path_thumb = args.dir + '/ffhq/train/'
     train_data_thumb = create_dataset(train_path_thumb, 64)
     train_loader = DataLoader(train_data_thumb, batch_size=4, shuffle=True)
 
